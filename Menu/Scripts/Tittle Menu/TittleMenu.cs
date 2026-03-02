cd33cb1c8e55803163194d193afa9f06d4508565
SJuniurr <auniradasilvaqueiroz@gmail.com>
2026-03-02 09:14:37 -0300

    Adicionei AnimationTree state machine para o title menu
    Introduzi uma state machine baseada em AnimationTree e novas animações (CBottom, CRight) na cena do title menu; ajustei as faixas de animação de entrada/saída e a AnimationLibrary para suportar transições suaves. Exponha os nodes AnimationTree, ButtonSFX e StartPressed como propriedades de cena exportadas e conectei-os em TittleMenu.cs: substitui o uso direto de AnimationPlayer por AnimationTree + AnimationNodeStateMachinePlayback, atualizei o HandleStartPressed para definir as condições de estado e acionar as transições e direcione o áudio através da nova exportação ButtonSFX. Ajustei também os deslocamentos/escalas de vários nós da interface do usuário para o layout.

--- Menu/Scripts/Tittle Menu/TittleMenu.cs
+++ Menu/Scripts/Tittle Menu/TittleMenu.cs
@@ -6,25 +6,26 @@ public partial class TittleMenu : Control
     [Export] private TextureRect _logo;
     [Export] private float rotationAmplitude = 3f;
     [Export] private float shakeSpeed = 15f;
+    [Export] private AudioStreamPlayer _ButtonSFX;
+    [Export] private StartPressed _startLogic;
+    [Export] private AnimationTree _animationTree;
 
     private IControlEffect _logoShake;
 
-    private StartPressed _startLogic;
-    private AnimationPlayer _animationPlayer;
-    private AudioStreamPlayer _sfx;
+    private AnimationNodeStateMachinePlayback _animationStateMachine;
 
     public override void _Ready()
     {
         MenuMusicPlayer.Instance.PlayMusic();
 
-        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-        _startLogic = GetNode<StartPressed>("StartPressed");
-        _sfx = GetNode<AudioStreamPlayer>("EnterSFX");
-
         _logoShake = new ShakeEffect(rotationAmplitude, shakeSpeed);
 
+        _animationStateMachine = (AnimationNodeStateMachinePlayback)_animationTree.Get("parameters/playback");
+        _animationTree.Set("parameters/conditions/CBottom", true);
+        _animationStateMachine.Travel("RESET");
+
         _startLogic.Starting += HandleStartPressed;
-        _animationPlayer.AnimationFinished += OnAnimationFinished;
+        _animationTree.AnimationFinished += OnAnimationFinished;
     }
 
     public override void _Process(double delta)
@@ -34,8 +35,10 @@ public partial class TittleMenu : Control
 
     private void HandleStartPressed()
     {
-        _sfx?.Play();
-        _animationPlayer.Play("Exit");
+        _ButtonSFX?.Play();
+        _animationTree.Set("parameters/conditions/CBottom", false);
+        _animationTree.Set("parameters/conditions/Exit", true);
+        _animationStateMachine.Travel("RESET");
     }
 
     private void OnAnimationFinished(StringName animName)
