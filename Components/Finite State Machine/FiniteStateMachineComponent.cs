a34f7c3e1d591dfa8e7a45f0bef53ace5f401fff
ElNeonnn <jndrsn.08@gmail.com>
2026-03-22 13:27:50 -0300

    ao entrar na árvore de cenas, adiciona os estados ao seu dicionário de estados

--- Components/Finite State Machine/FiniteStateMachineComponent.cs
+++ Components/Finite State Machine/FiniteStateMachineComponent.cs
@@ -1,6 +1,15 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class FiniteStateMachineComponent : Node
 {
+    private Dictionary<string, State> states = [];
+
+    public override void _Ready()
+    {
+        foreach (var child in GetChildren())
+            if (child is State state)
+                states[state.Name.ToString().ToLower()] = state;
+    }
 }
