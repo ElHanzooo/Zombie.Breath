75d50e374584efcfe836119caa3a617ac19cfa1a
ElNeonnn <jndrsn.08@gmail.com>
2026-03-22 13:43:12 -0300

    criei o método OnStateTransition. Ele é usado quando o evento Transitioned é emitido. Ele é usado para trocar de estado

--- Components/Finite State Machine/FiniteStateMachineComponent.cs
+++ Components/Finite State Machine/FiniteStateMachineComponent.cs
@@ -4,12 +4,39 @@ using System.Collections.Generic;
 
 public partial class FiniteStateMachineComponent : Node
 {
+    private State currentState;
     private Dictionary<string, State> states = [];
 
     public override void _Ready()
     {
         foreach (var child in GetChildren())
             if (child is State state)
+            {
                 states[state.Name.ToString().ToLower()] = state;
+                state.Transitioned += OnStateTransition;
+            }
+    }
+
+    public override void _Process(double delta)
+    {
+        currentState?.Update((float)delta);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        currentState?.PhysicsUpdate((float)delta);
+    }
+
+    private void OnStateTransition(string newStateName)
+    {
+        if (!states.TryGetValue(newStateName.ToLower(), out var newState))
+            return;
+
+        if (currentState == newState)
+            return;
+
+        currentState?.Exit();
+        newState.Enter();
+        currentState = newState;
     }
 }
