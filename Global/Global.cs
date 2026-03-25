db6a3705ef870eae9fae6bf07b5ea87e0416794c
ElNeonnn <jndrsn.08@gmail.com>
2026-03-25 10:12:25 -0300

    criei as propriedades Act e Episode. Todo Act tem um Episode Day, Afternoon e Night. Após terminar um Act, um novo é carregado no Episode Day

--- Global/Global.cs
+++ Global/Global.cs
@@ -3,4 +3,20 @@ using System;
 
 public partial class Global : Node
 {
+    public static Global Instance { get; private set; }
+
+    public int Act = 1;
+    public Episodes Episode = Episodes.Day;
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+}
+
+public enum Episodes
+{
+    Day,
+    Afternoon,
+    Night
 }
