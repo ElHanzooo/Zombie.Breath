3271191c4da201e42afc3d25c2ced87e1269920a
ElNeonnn <jndrsn.08@gmail.com>
2026-03-29 20:37:37 -0300

    removi o debug de IsLookingUp

--- Characters/Mike/Mike.cs
+++ Characters/Mike/Mike.cs
@@ -5,9 +5,4 @@ public partial class Mike : Area2D
 {
     public bool NeedReload = false;
     public bool IsLookingUp = false;
-
-    public override void _Process(double delta)
-    {
-        GD.Print(IsLookingUp);
-    }
 }
