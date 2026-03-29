90c4b3b48f246ac9d3e58fee216b2487304108ab
ElNeonnn <jndrsn.08@gmail.com>
2026-03-29 20:35:55 -0300

    posiciona o Mike ao iniciar o jogo

--- Gameplay/Gameplay.cs
+++ Gameplay/Gameplay.cs
@@ -4,10 +4,14 @@ using System;
 public partial class Gameplay : Node2D
 {
     [Export] private CanvasLayer Background { get; set; }
+    [Export] private CanvasLayer Characters { get; set; }
 
     public override void _Ready()
     {
         var background = Background.GetNode<AnimatedSprite2D>("Background");
         background.Position = GetViewportRect().GetCenter();
+
+        var mike = Characters.GetNode<Area2D>("Mike");
+        mike.Position = new Vector2(135, 495);
     }
 }
