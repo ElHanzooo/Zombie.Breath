86605055449f1c13bb602f54a8a4e912d0bd9396
ElNeonnn <jndrsn.08@gmail.com>
2026-03-29 20:32:20 -0300

    centraliza o background ao iniciar o jogo

--- Gameplay/Gameplay.cs
+++ Gameplay/Gameplay.cs
@@ -3,4 +3,11 @@ using System;
 
 public partial class Gameplay : Node2D
 {
+    [Export] private CanvasLayer Background { get; set; }
+
+    public override void _Ready()
+    {
+        var background = Background.GetNode<AnimatedSprite2D>("Background");
+        background.Position = GetViewportRect().GetCenter();
+    }
 }
