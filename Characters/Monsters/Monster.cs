6b89278c02d0abc1519350392ede59de4847c57d
ElNeonnn <jndrsn.08@gmail.com>
2026-04-21 09:39:14 -0300

    criei as propriedades que todo monstro terá: Health (vida), AttackDamage (dano do ataque) e Speed (velocidade). Os valores pré-definidos são os valores padrão de um monstro Zumbi

--- Characters/Monsters/Monster.cs
+++ Characters/Monsters/Monster.cs
@@ -1,7 +1,9 @@
 using Godot;
 using System;
 
-public abstract partial class Monster
+public abstract partial class Monster : Area2D
 {
-    
+    [Export] public int Health { get; protected set; } = 50;
+    [Export] public int AttackDamage { get; protected set; } = 30;
+    [Export] public float Speed { get; protected set; } = 1;
 }
