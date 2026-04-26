a198fb3fb287255a726b242d000f12e131eaca78
SJuniurr <auniradasilvaqueiroz@gmail.com>
2026-04-26 16:40:11 -0300

    Substitui as nuvens do AnimationPlayer por um script de tween.
    Removi os recursos de animação/AnimationPlayer e a biblioteca de animação de main_menu.tscn e converti Cloud em um TextureRect com script (adicionei layout, deslocamentos, escala, textura, script e array cloudTextures). Adicionei o node Cloud2. Atualizei Cloud.cs para substituir as animações controladas por AnimationPlayer por uma PlayCloudAnimation baseado em Tween que randomiza a textura, a escala, a posição vertical e o atraso inicial, e então interage com a nuvem na tela; adicionei as propriedades Min/MaxTime e Min/MaxScale exportadas e simplifiquei o tempo usando temporizadores GetTree e funções aleatórias do GD.

--- Menu/Scripts/Main Menu/Cloud.cs
+++ Menu/Scripts/Main Menu/Cloud.cs
@@ -5,26 +5,44 @@ using System.Threading.Tasks;
 
 public partial class Cloud : TextureRect
 {
-    [Export] private AnimationPlayer animationPlayer;
+    [Export] private float MaxTime = 9f;
+    [Export] private float MinTime = 5f;
+    [Export] private float MaxScale = 0.25f;
+    [Export] private float MinScale = 0.2f;
     [Export] private Array<Texture2D> cloudTextures;
 
-    private Random _random = new Random();
-
+    private Tween _tween;
+    
     public override void _Ready()
     {
-        animationPlayer.AnimationFinished += OnAnimationFinished;
+        PlayCloudAnimation();        
     }
 
-    private async void OnAnimationFinished(StringName animName)
+    private async void PlayCloudAnimation()
     {
-        if (animName == "Cloud")
-        {
-            Texture = cloudTextures[_random.Next(cloudTextures.Count)];
-            animationPlayer.SpeedScale = (float)_random.NextDouble() * 0.5f + 0.5f; // Random speed between 0.5 and 1.0
+        await ToSignal(GetTree().CreateTimer((float)GD.RandRange(0.5f, 2f)), "timeout");
 
-            await ToSignal(GetTree().CreateTimer(_random.Next(0, 5)), "timeout");
-            
-            animationPlayer.Play("Cloud");
+        float textureWidth = Texture.GetWidth();
+        Position = new Vector2(-textureWidth, (float)(GD.RandRange(100f, GetViewportRect().Size.Y / 2.5f))); 
+
+        Texture = cloudTextures[(int)(GD.Randi() % cloudTextures.Count)];
+        Scale = new Vector2((float)GD.RandRange(MinScale, MaxScale), (float)GD.RandRange(MinScale, MaxScale));
+
+        _tween?.Kill();
+        _tween = GetTree().CreateTween();
+
+        float duration = (float)GD.RandRange(MinTime, MaxTime);
+        float screenWidth = GetViewportRect().Size.X;
+
+        _tween.TweenProperty(this, "position:x", screenWidth, duration)
+              .SetTrans(Tween.TransitionType.Linear)
+              .SetEase(Tween.EaseType.InOut);
+        
+        await ToSignal(_tween, "finished");
+
+        if (IsInstanceValid(this))
+        {
+            PlayCloudAnimation();
         }
     }
 }
